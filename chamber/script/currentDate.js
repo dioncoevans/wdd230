4e99606e5726af2e1d242a40f25c629b0dbad9a0
dioncoevans <eva17028@byui.edu>
2022-05-12 21:31:22 -0600

    css added

---
+++ chamber/script/currentDate.js
@@ -0,0 +1,7 @@
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
+document.querySelector('#currentDate').textContent = new Date().toLocaleDateString('en-US', options);
