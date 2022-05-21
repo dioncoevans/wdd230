86c2441a0795515679c08ae214c85cb700c65ea8
dioncoevans <eva17028@byui.edu>
2022-05-21 13:20:39 -0600

    BOM top 25

--- chamber/script/lastMod.js
+++ chamber/script/lastMod.js
@@ -1,3 +1,21 @@
+// similar
+window.addEventListener('load', (event) => {
+    const lastMod = document.querySelector('#lastMod');
+    //uses the function for lastModified, a built in JS function
+    lastMod.textContent = document.lastModified;
+
+    const cry = document.querySelector('#copyRightYear');
+    cry.textContent = new Date().getFullYear();
+})
+
+
+
+
+
+
+
+
+
 /*const options = {
 
 day: 'numeric',
@@ -9,7 +27,7 @@ seconds: 'numeric',
 }
 document.querySelector("lastMod").textContent = new Date(lastModified).toLocaleDateString('en-US', options);*/
 
-const thisYear = new Date();
+/*const thisYear = new Date();
 let year = thisYear.getFullYear();
 document.getElementById("year").textContent = year;
 
@@ -17,7 +35,7 @@ const lastMod = new Date(document.lastModified);
 let strLMD = 'Last updated: ' + lastModDate.getMonth() + "/" + lastModDate.getDate() + "/" 
            + lastModDate.getFullYear() + " " + lastModDate.getHours() + ":" 
            + lastModDate.getMinutes() + ":" + lastModDate.getSeconds();
-document.getElementById("modified").textContent = strLMD;
+document.getElementById("modified").textContent = strLMD;*/
 
 /*const thisYear = new Date();
 let year = thisYear.getFullYear();
