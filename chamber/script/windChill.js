009c654eb5dc9cce137780ba90f1a2635842d098
dioncoevans <eva17028@byui.edu>
2022-05-26 11:01:53 -0600

    weather

---
+++ chamber/script/windChill.js
@@ -0,0 +1,12 @@
+const temp = parseFloat(document.getElementById('degrees').innerHTML);
+const windS = parseFloat(document.getElementById('windSpeed').innerHTML);
+
+if(temp >= 50 || windS < 3){
+    document.getElementById('windChill').innerHTML = "N/A";
+}
+else{
+const windCh = Math.round(35.74 +  (0.6215 * temp) - (35.75 * windS ^ 0.16) + (0.4275 * temp * windS ^ 0.16));
+document.getElementById('windChill').innerHTML = windCh;}
+
+//Math.round(35.74 +  (0.6215 * temp) - (35.75 * (windS ^ 0.16)) + (0.4275 * (temp * (windS ^ 0.16))));
+//document.getElementById('windChill').innerHTML = windCh;}
