c3fe6b4235193c12456e0542855b7ada7d2d8e89
dioncoevans <eva17028@byui.edu>
2022-05-27 10:23:45 -0600

    sizes hero image

--- chamber/script/windChill.js
+++ chamber/script/windChill.js
@@ -5,7 +5,7 @@ if(temp >= 50 || windS < 3){
     document.getElementById('windChill').innerHTML = "N/A";
 }
 else{
-const windCh = Math.round(35.74 +  (0.6215 * temp) - (35.75 * windS ^ 0.16) + (0.4275 * temp * windS ^ 0.16));
+const windCh = Math.round(35.74 +  (0.6215 * temp) - (35.75 *  Math.pow(windS, 0.16)) + (0.4275 * (temp * Math.pow(windS,0.16))));
 document.getElementById('windChill').innerHTML = windCh;}
 
 //Math.round(35.74 +  (0.6215 * temp) - (35.75 * (windS ^ 0.16)) + (0.4275 * (temp * (windS ^ 0.16))));
